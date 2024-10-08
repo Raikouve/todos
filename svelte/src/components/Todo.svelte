aa4df3e8318bd8392bf12e1a2296eff973b99011
Antônio Gonçalves <79341076+Raikouve@users.noreply.github.com>
2024-10-08 20:18:22 -0300

    Add svelte implementation

---
+++ svelte/src/components/Todo.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from "svelte";
+
+  let todos = [];
+  let todoText = "";
+
+  onMount(() => {
+    const existingTodos = localStorage.getItem('todos');
+    todos = JSON.parse(existingTodos || []);
+  });
+
+  function addTodo() {
+    todos = [...todos, todoText];
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+</script>
+
+
+<div>
+  <div>
+    <input type="text" placeholder="Tarefa" bind:value={todoText} />
+    <button id="btn-add-todo" on:click={addTodo}>+</button>
+  </div>
+  <ul id="todo-list">
+    {#each todos as todo}
+      <li>{todo}</li>
+    {/each}
+  </ul>
+</div>
---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Todo from "../components/Todo.svelte";
+</script>
+
+<main>
+  <Todo />
+</main>
